778360dd13d192dc3c38061a8598a0f47b2a2cdb
JaramilloChristian28 <jaramillochristian413@gmail.com>
2024-05-04 07:55:20 +0800

    Project Folder

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function addToCart(Request $request)
+    {
+        // Logic to add item to cart
+    }
+
+    public function removeFromCart(Request $request)
+    {
+        // Logic to remove item from cart
+    }
+}
