00047a6bbde108014fa94333dcac71980682b60f
JaramilloChristian28 <jaramillochristian413@gmail.com>
2024-05-10 00:42:09 +0800

    backend

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    protected $fillable = ['user_id'];
-
-    public function items()
-    {
-        return $this->hasMany(CartItem::class);
-    }
+    use HasFactory;
+    protected $table='cart';
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'qty',
+    ];
 }
